34ba9ad209f83757144b6887fe825e1ca66d98ca
kocjanr <kocjanr@onid.oregonstate.edu>
2019-05-28 21:42:52 -0700

    worked up till step 7

--- examples/wine.py
+++ examples/wine.py
@@ -14,5 +14,16 @@ from sklearn.metrics import mean_squared_error,r2_score
 import joblib
 
 data = pd.read_csv(DATASET_URL,sep=';')
-# print(data.head())
-# print(data.describe())
+
+y = data.quality
+X = data.drop('quality',axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=123,stratify=y)
+
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+pipeline = make_pipeline(preprocessing.StandardScaler(),RandomForestRegressor(n_estimators=100))
+
+hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
+                  'randomforestregressor__max_depth': [None, 5, 3, 1]}
